daf33fa538e9e07b870848eecec0222ce1036bf1
Wesley Santos <wesleylelo0078@gmail.com>
2021-03-12 15:20:09 -0300

    Projeto criado

---
+++ src/main/java/com/mycompany/projetojava/ProgramJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetojava;
+
+/**
+ *
+ * @author lelo0
+ */
+public class ProgramJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
