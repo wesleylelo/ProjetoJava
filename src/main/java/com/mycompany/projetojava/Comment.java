2901887250640ff00dee79c6f7cbb593c6ae1aa7
Wesley Santos <wesleylelo0078@gmail.com>
2021-03-16 12:29:56 -0300

    EXERCICIOS SOCIAL MEDIA

---
+++ src/main/java/com/mycompany/projetojava/Comment.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetojava;
+
+/**
+ *
+ * @author lelo0
+ */
+public class Comment {
+    private String text;
+    
+    public Comment(){
+        
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/projetojava/Post.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetojava;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author lelo0
+ */
+public class Post {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer likes;
+    
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post(Date moment, String title, String content, Integer likes) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+    public Post(){
+        
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+   public void addComment(Comment comment){
+       comments.add(comment);
+   }
+   
+   public void removeComment(Comment comment){
+       comments.remove(comment);
+   }
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(title + "\n");
+        sb.append(likes);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append(content + "\n");
+        sb.append("Comments:\n");
+        for(Comment c: comments){
+            sb.append(c.getText() + "\n");
+        }
+        return sb.toString();
+    }
+    
+}
