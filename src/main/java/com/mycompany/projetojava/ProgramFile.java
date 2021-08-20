dfc98c583a472f984e5721ed862854631b6d6eec
Wesley <wesleylelo0078@gmail.com>
2021-08-20 15:29:13 -0300

    Abrindo Arquivo -try-with-resoures- --

--- src/main/java/com/mycompany/projetojava/ProgramFile.java
+++ src/main/java/com/mycompany/projetojava/ProgramFile.java
@@ -23,11 +23,8 @@ public class ProgramFile {
     public static void main(String[] args) {
         // TODO code application logic here
         String path = "C:\\Users\\lelo0\\OneDrive\\Área de Trabalho\\COMANDOSCISCO.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+        
             String line = br.readLine();
             while(line != null){
                 System.out.println(line);
@@ -36,19 +33,6 @@ public class ProgramFile {
         }
         catch(IOException e){
             System.out.println("Error: " + e.getMessage());
-        }
-        finally{
-            try{
-                if(br != null){
-                    br.close();
-                }
-                if(fr != null){
-                    fr.close();
-                }
-        
-            } catch(IOException e){
-                e.printStackTrace();
-            }
         }
     }
     
